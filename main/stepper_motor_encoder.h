139e0ae8584ae4091c2e268ea04f831cc691100a
islingr896 <islingr896@gmail.com>
2025-12-12 00:13:19 +0700

    update stepper_motor_encoder.h file

--- main/stepper_motor_encoder.h
+++ main/stepper_motor_encoder.h
@@ -28,3 +28,32 @@ typedef struct
 {
     uint32_t resolution; // Encoder resolution, in Hz
 }stepper_motor_uniform_encoder_config_t;
+
+
+/**
+ * @brief Create stepper motor curve encoder
+ *
+ * @param[in] config Encoder configuration
+ * @param[out] ret_encoder Returned encoder handle
+ * @return
+ *      - ESP_ERR_INVALID_ARG for any invalid arguments
+ *      - ESP_ERR_NO_MEM out of memory when creating step motor encoder
+ *      - ESP_OK if creating encoder successfully
+ */
+esp_err_t rmt_new_stepper_motor_curve_encoder(const stepper_motor_curve_encoder_config_t *config, rmt_encoder_handle_t *ret_encoder);
+
+/**
+ * @brief Create RMT encoder for encoding step motor uniform phase into RMT symbols
+ *
+ * @param[in] config Encoder configuration
+ * @param[out] ret_encoder Returned encoder handle
+ * @return
+ *      - ESP_ERR_INVALID_ARG for any invalid arguments
+ *      - ESP_ERR_NO_MEM out of memory when creating step motor encoder
+ *      - ESP_OK if creating encoder successfully
+ */
+esp_error_t rmt_new_stepper_motor_uniform_encoder(const stepper_motor_uniform_encoder_config_t *config, rmt_channel_handle_t *ret_encoder);
+
+#ifdef __cplusplus
+}
+#endif
